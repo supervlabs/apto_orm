3dd216c564f062d621524c71240c047e5b74614d
willing <willing@supervlabs.io>
2024-09-20 10:45:25 +0900

    updated aptos move command for move v2

--- typescript/src/sdk/packages.ts
+++ typescript/src/sdk/packages.ts
@@ -141,7 +141,7 @@ export function compilePackage(config: Pick<OrmPackageConfig, 'package_move_path
       'install aptos-cli through the command `curl -fsSL "https://aptos.dev/scripts/install_cli.py" | python3`'
     );
   }
-  let command = `cd ${package_move_path} && aptos move compile --move-2 --bytecode-version 6 --save-metadata`;
+  let command = `cd ${package_move_path} && aptos move compile --move-2 --save-metadata`;
   const address = Object.entries(named_addresses || {}).map(([name, addr]) => `${name}=${addr}`);
   if (address.length > 0) command = command + ` --named-addresses ${address.join(',')}`;
   try {
